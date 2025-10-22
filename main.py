d6fa51a96c552a3725165cb4f536efd23935a8ce
GGs121 <161675306+GGs121@users.noreply.github.com>
2025-10-22 12:54:49 -0500

    Main(gage)

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 def main():
-    pass
+    
+    def menu():
+        print('WELCOME TO THE NUMBER GUESSING GAME!!!')
+        print('--------------------------------------')
+        print('MAIN MENU')
+        print('--------------------------------------')
+        print('START NEW GAME (1)')
+        print('CHOOSE RANGE (2)')
+        print('EXIT (3)')
+        choice = int(input())
+        return choice
+    
+    if choice == 1:
+        
+    menu()
