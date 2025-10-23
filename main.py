46045a65b773604d632b960a1ce63187a041e0f1
GGs121 <161675306+GGs121@users.noreply.github.com>
2025-10-23 12:43:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 def main():
     
     def menu():
-        print('WELCOME TO THE NUMBER GUESSING GAME!!!')
-        print('--------------------------------------')
-        print('MAIN MENU')
-        print('--------------------------------------')
-        print('START NEW GAME (1)')
-        print('CHOOSE RANGE (2)')
-        print('EXIT (3)')
-        choice = int(input())
-        return choice
+
     
-    if choice == 1:
+        if choice == 1:
+            
+            getnames()
+            start_new_game()
+        elif choice == 2:
+            
+            setrang()
+        else:
+            stopfunc()
+        
+    
         
     menu()
+    
+def start_new_game(minrange, maxrange, name1, name2, randnum):
+    win = 1
+    totalturns = 0
+    while win == 1:
+        player1g = int(input('\n',name1,'guees your number inbetween',minrange, 'and', maxrange))
+        
+        if player1g == randnum:
+            print('WINNER!!!')
+            win = 0
+        if player1g > randum:
+            print('guess lower')
+            print()
+            
+        if player1g < randum:
+            print('guess higher')
+            print()
+            
+        player2g = int(input('\n',name2,'guees your number inbetween',minrange, 'and', maxrange))
+        
+        if player2g == randnum:
+            print('WINNER!!!')
+            win = 0
+            
+        if player2g > randum:
+            print('guess lower')
+            print()
+            
+        if player2g < randum:
+            print('guess higher')
+            print()
+            
+        totalturns = totalturns + 1
+        print('TURN COUNT: ',totalturns)
+        
+main()
